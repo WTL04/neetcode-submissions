eb9aa76bf3bc6f67f4f23c13268a8efec2e11894
William Luu <114207423+WTL04@users.noreply.github.com>
2026-09-22 20:37:31 -0700

    Add: minimum-size-subarray-sum - submission-1

---
+++ Data Structures & Algorithms/minimum-size-subarray-sum/submission-1.py
@@ -0,0 +1,59 @@
+class Solution:
+    def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+        """
+        UMPIRE
+        understand - smallest possible array that have greater than or equal to sum to the target
+
+        match - sliding window due to finding a subarray, dynamic due to finding minimum size
+
+        plan - 
+
+        left, right = 0, 0
+        running_total = 0
+
+        minimum_length = inf
+
+        
+        expand window until end of array
+            if subarray is greater than or equal to target
+                record current window length 
+                compare to minimum_length
+                shrink window
+
+        if minimum_length hasn't changed:
+            return 0, there is no subarray
+
+        return minimum_length
+
+        review - edge cases:
+            1. no such subarray, return 0
+        """
+        n = len(nums)
+        l, r = 0, 0
+        total = 0
+        min_length = 9999999999
+
+        for r in range(n):
+            total += nums[r]
+            
+            # shrink window
+            while total >= target:
+                # record min window length
+                win_length = (r - l) + 1
+                min_length = min(win_length, min_length)
+
+                # shrink window
+                total -= nums[l]
+                l += 1
+
+        if min_length == 9999999999:
+            return 0 
+
+        return min_length
+
+        """
+        evaluate:
+            time: O(n), where n is the lenght of nums, sliding window means 1 pass
+            space: O(1), nothing stored in memory
+        """
+
